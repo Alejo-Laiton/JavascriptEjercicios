e10cdf11dc97ed1f8acbb4868fcb23c2e12128a1
Alejo-Laiton <alejandrolaiton33@gmail.com>
2023-04-08 18:41:37 -0500

    Ejercicio de clase con listas y objetos

---
+++ ejercicioTema2/lista.js
@@ -0,0 +1,12 @@
+//Lista sobre mi
+
+const libroFav = {
+    titulo: "La travesia del viajero del Alba",
+    autor: "C. S. Lewis",
+    fecha: new Date(1952),
+    url: "https://es.wikipedia.org/wiki/The_Voyage_of_the_Dawn_Treader#:~:text=The%20Voyage%20of%20the%20Dawn%20Treader%20(traducido%20como%20La%20traves%C3%ADa,Lewis%20en%201952."
+    
+}
+const lista = [ "Alejo Laiton", 26, true, Date(1997, 0, 16), libroFav ]
+
+
