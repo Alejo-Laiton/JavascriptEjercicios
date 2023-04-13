927343d5d854ce889547bf9fff8e5dc0363c3bfd
Alejo-Laiton <alejandrolaiton33@gmail.com>
2023-04-13 16:25:13 -0500

    Ejercicio de clase con números

---
+++ ejercicioTema5/numeros-Js.js
@@ -0,0 +1,12 @@
+// Ejercicio números ejercicio 5
+
+let altura_cm = 172;
+let altura_m = 1.72;
+let peso_kg = 80.4;
+let altura_m_red = altura_m.toPrecision(1);
+let peso_kg_red = peso_kg.toFixed(0);
+let maxValorJs_1 = Number.Max_VALUEA + 1;
+
+console.log(altura_m_red)
+console.log(peso_kg_red)
+console.log(maxValorJs_1)
