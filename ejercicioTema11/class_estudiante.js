e3100166b42f64e598708e694de1c75a9ee6f806
Alejo-Laiton <alejandrolaiton33@gmail.com>
2023-04-28 11:29:20 -0500

    Ejercicio de clase con Clases

---
+++ ejercicioTema11/class_estudiante.js
@@ -0,0 +1,16 @@
+class Estudiante {
+    
+    constructor(nombre) {
+        this.nombre = nombre;
+        this.asignaturas = [ "JavaScript", "HTML", "CSS" ];
+    }
+    obtenDatos() {
+        return {
+            nombre: this.nombre, 
+            asignaturas: this.asignaturas 
+        }
+    }
+}
+
+const estudiante1 = new Estudiante("Alejo")
+console.log(estudiante1.obtenDatos())
