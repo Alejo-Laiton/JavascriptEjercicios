d9777d4b1d75c4fc73b5e83fdd5b2ad4d82c0a35
Alejo-Laiton <alejandrolaiton33@gmail.com>
2023-05-01 17:44:32 -0500

    Ejercicio de clase con ESLint

---
+++ ejercicioTema13/Ejerccio-ESLint.js
@@ -0,0 +1,24 @@
+// Ejercicio 4 - usando ESLint
+
+let nombre = "  Alejandro  "
+let apellido = "  Laiton  "
+let name = nombre.trim()
+let lastname = apellido.trim()
+let estudiante = `${name} ${lastname}`
+// let estudiante = name.concat(" ", lastname)
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let estudianteLarge = estudiante.length
+let primerCaracter = estudiante.charAt(`${name}`)
+let indexLastnameEnd = estudiante.indexOf(`${lastname}`) + lastname.length - 1
+let ultimoCaracter = estudiante.charAt(`${indexLastnameEnd}`)
+let estudianteTrim = estudiante.replace(/\s/g,"")
+
+console.log(estudiante)
+console.log(estudianteMayus)
+console.log(estudianteMinus)
+console.log(estudianteLarge)
+console.log(primerCaracter)
+console.log(indexLastnameEnd)
+console.log(ultimoCaracter)
+console.log(estudianteTrim)
