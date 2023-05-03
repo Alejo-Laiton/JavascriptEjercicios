9d7d14594db50a04088144ff806401dd1f0924e2
Alejo-Laiton <alejandrolaiton33@gmail.com>
2023-05-03 17:38:21 -0500

    Ejercicio en clase con Persistencia de datos en el navegador

---
+++ ejercicioTema15/index.js
@@ -0,0 +1,15 @@
+const nombre = "Alejo"
+const apellido = "Perez"
+
+    const persona = {
+        nombre,
+        apellido
+    }   
+
+// sessionStorage.setItem("usuario", JSON.stringify(persona))
+// localStorage.setItem("usuario", JSON.stringify(persona))
+
+// const expiration_date = new Date();
+// expiration_date.setTime(expiration_date.getTime() + (2 * 60 * 1000)) 
+
+// document.cookie = `nombrecookie=${JSON.stringify(persona)}; expires=` + expiration_date.toUTCString() + ";path=/"
